451a9cca6c60367eca9010e9f8510457e6f5caac
Chris Hampu <chrishampu@outlook.com>
2016-03-13 11:21:20 +0000

    More progress many hours later

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,3 +1,207 @@
+var Restaurants = [
+    {
+        name: "Original Joes",
+        location: ["1654 McKenzie Avenue, Victoria, BC V8N 0A3"],
+        type: "western",
+        budget: 15
+    },
+    {
+        name: "1550s",
+        location: ["1550 Cedar Hill X Rd, Victoria, BC V8P 2P4"],
+        type: "western",
+        budget: 15
+    },
+    
+    {
+        name: "White Spot",
+        location: ["3965 Quadra St, Victoria, BC V8X 1J8"],
+        type: "western",
+        budget: 12
+    },
+    
+    {
+        name: "Boston Pizza",
+        location: ["797 Hillside Ave #10, Victoria, BC V8T 1Z5"],
+        type: "western",
+        budget: 12
+    },
+    
+    
+    {
+        name: "Phonomenal",
+        location: ["Suite 129, 3749 Shelbourne St, Victoria, BC V8P 5N4"],
+        type: "asian",
+        budget: 10
+    },
+    
+    {
+        name: "East Garden Restaurant(东篱江湖菜)",
+        location:"2888 Belmont Ave",
+        type: "asian",
+        budget: 14
+        
+    },
+    
+    {
+        name: "Sun Wah Chinese Restaurant",
+        location:"1515 Cook St, Victoria, BC V8T 5E5",
+        type: "asian",
+        budget: 14
+    },
+    
+    {
+        name: "Sun Wah Chinese Restaurant",
+        location:"1515 Cook St, Victoria, BC V8T 5E5",
+        type: "asian",
+        budget: 14
+
+    }
+    
+    
+    
+    
+];
+
+
+var Expected_food_input = ["chinese", "indian", "asian", "thai", "japanese", 
+                            "western", "italian", "greek", "malaysian"]
+                            
+var Expected_budget_input = ['$5', '$10', '$15', '$20', '$25'];                           
+
+var Expected_context_input = ['eat', 'play', 'sing', 'drink', 'watch'];
+
+// global
+var If = React.createClass({
+	render: function() {
+		if (this.props.test) {
+			return this.props.children;
+		} else {
+			return false;
+		}
+	}
+});
+
+
+var Result = React.createClass({
+   render: function() { 
+       
+       return ( 
+       
+       <div className="col-md-4">
+            <div className="result-card">
+                <div id= "restaurant-name">{this.props.data.name}</div>
+                <div id= "restaurant-type">{this.props.data.type} Cuisine</div>
+                <div>{this.props.data.location}</div>
+                <div>${this.props.data.budget} Budget</div>
+            </div>
+        </div>
+    )
+   } 
+});
+
+
+var ResultView = React.createClass({ 
+   getInitialState: function() {
+     return {
+         mapHeight: undefined
+     }  
+   },
+   componentWillMount: function() {
+
+   },
+   componentDidMount: function() {
+       
+       this.updateMapHeight(); 
+       
+        this.map = new google.maps.Map(ReactDOM.findDOMNode(this.refs.map), {
+            center: {lat: 48.448398, lng: -123.338847}, 
+            zoom: 12
+        });
+
+        var that = this;
+
+        window.addEventListener('resize', this.updateMapHeight);
+        
+        for(var i = 0; i < this.props.data.length; i++) {
+            this.addGeocodedMarker(this.props.data[i].location[0]);
+        }
+
+   },
+   componentWillUnmount: function() {
+       
+        window.removeEventListener('resize', this.updateMapHeight);
+   },
+   updateMapHeight: function() {
+       var top = 0;
+        var element = this.refs.map;
+        var viewportHeight = document.documentElement.clientHeight;
+       
+        do {
+            top += element.offsetTop  || 0;;
+            element = element.offsetParent;
+        } while(element);
+
+        this.offsetTop = viewportHeight - top - 30;
+        
+        this.setState({mapHeight: this.offsetTop});
+   },
+  addGeocodedMarker: function(address) {
+      
+      var geocoder = new google.maps.Geocoder();
+      var that = this;
+      
+      
+        geocoder.geocode({'address': address}, function(results, status) {
+            
+            if (status === google.maps.GeocoderStatus.OK) {
+                
+                console.log(results[0].geometry.location);
+                
+                var marker = new google.maps.Marker({
+                    map: that.map,
+                    position: results[0].geometry.location
+                });
+            } else {
+                console.log(status);
+            }
+          
+        });
+  },
+   render: function() {
+       
+       var height = {
+            height: this.state.mapHeight !== undefined ? this.offsetTop  + "px" : 0
+       };
+       
+       return (
+            <div className="result-container full-size">
+                <div className="container-fluid full-size">
+                    <div className="row full-size">
+                        <div className="col-md-6">
+                            <If test={this.props.data.length > 0}>
+                                <div className="container-fluid">
+                                    <div className="row card-container">
+                                    
+                                    { this.props.data.map(function(val, i) {
+                                        return <Result key={i} data={val}/>
+                                    }) }
+                                    
+                                    </div>
+                                </div>
+                            </If>
+                            { this.props.data.length==0 ? <div className="no-result">No results</div> : false }
+                        </div>
+                        <div className="col-md-6 map-container" style={ height }>
+                            <div ref="map" id="map-container" style={ height }></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+   }
+});
+
+//Entry
 var Splash = React.createClass({
     getInitialState: function() {
         return {
@@ -28,13 +232,13 @@ var Splash = React.createClass({
     render: function() {
         return (
             <div id="splash-page" className="full-size">
-                <div className="inner-middle ">
-                    <div id="splash-content" className="horizontal-middle">
+                <div className="full-size">
+                    <div id="splash-content" className="horizontal-middle full-size">
                         <div className="splash-title">
                             <h2>Hi there </h2>
                             <h2>The time is currently {this.state.dateString} in Victoria</h2>
                         </div>
-                        <div>
+                        <div className="full-size">
                             <button type="button" onClick={this.handleButtonClick} className="btn btn-default btn-large splash-button" aria-label="Left Align">
                                 <span className="glyphicon glyphicon-search" aria-hidden="true"></span>
                                 What would you like to do ?
@@ -59,15 +263,17 @@ var SearchPrompt = React.createClass({
         if(ev.keyCode === 13) {
             
             this.props.promptFunctor(this, this.refs.searchinput.value);
+            
+            this.refs.searchinput.value = "";
         }
     },
     render: function() {
         return (
          <div className="search-box">
-         { this.state.value === undefined ?
+         { this.props.promptValue === undefined ?
             <div>
                 <div className="search-title">
-                I want to...{this.props.promptType}
+                {this.props.promptType}
                 </div>
                 <div className="form-inline">
                     <div className="form-group">
@@ -79,7 +285,7 @@ var SearchPrompt = React.createClass({
                 </div>
             </div>
             : 
-            <h2>What you're doing {this.props.promptType} is {this.state.value}</h2>
+            <h2>{this.props.promptType} {this.props.promptValue}</h2>
          }
         </div>
         )
@@ -88,8 +294,108 @@ var SearchPrompt = React.createClass({
 
 var EatContext = React.createClass({
     
+    getInitialState: function() {
+      
+        return {
+            foodType: undefined,
+            budget: undefined,
+            answers: [],
+            results: undefined,
+            errorMsg: undefined
+        }  
+    },
+    componentWillMount: function() {
+        
+        this.props.updateSuggestions(Expected_food_input, this.setFoodType);
+    },
+    setFoodType: function(prompt, nuValue) {
+       
+        var found = false;
+       
+        for(var i=0; i<Expected_food_input.length; i++){
+            
+            //if the input is not equal
+              if (nuValue == Expected_food_input[i]) {
+                    found = true;
+                    break;
+              }
+        }
+        
+        if(found) {
+            
+            this.state.answers.push({text:"I'm looking for", value: nuValue});
+            this.setState({foodType: nuValue, errorMsg: undefined});
+            this.props.updateSuggestions(Expected_budget_input, this.setBudget);
+            
+        } else {
+            
+            this.setState({errorMsg: "Failed to find " + nuValue + " cuisine in the database"});
+        }
+    },
+    setBudget: function(prompt, nuValue) {
+        
+        nuValue = nuValue.replace(/^\$/, "");
+
+        var pattern = /^\d+(.\d{1,2})?$/;
+        
+        if(nuValue.length === 0 || pattern.test(parseInt(nuValue)) === false) {
+             this.setState({errorMsg: "Not a valid budget. Try a monetary value."});
+        }
+        else {
+            this.state.answers.push({text:"My budget is", value: "$" + parseInt(nuValue)});
+            
+            this.setState({budget: nuValue, errorMsg: undefined}, function() {
+                this.gatherData();
+                this.props.updateSuggestions([]);
+            });
+        }    
+    },
+    gatherData: function() {
+        
+        var res = [];
+        
+        for(var i = 0; i < Restaurants.length; i++) {
+            
+            if(this.state.foodType.toLowerCase() === Restaurants[i].type.toLowerCase() && 
+                this.state.budget <= Restaurants[i].budget) {
+                    
+                res.push(Restaurants[i]);
+            }
+        }
+        
+        this.setState({results: res});
+        
+        
+    },
     render: function() {
-        return (<div>text</div>)
+        return (
+            <div className="full-size">
+      
+            { this.state.answers.map(function(val, i) {
+                
+                   return <SearchPrompt key={i} promptType={val.text} promptValue={val.value}/>
+     
+                }.bind(this))
+            }
+            
+            <If test={this.state.foodType===undefined || this.state.budget === undefined}>
+                 { this.state.foodType === undefined ?
+                    <SearchPrompt promptType="What kind of food?" promptFunctor={this.setFoodType}/>
+                    :
+                    <SearchPrompt promptType="What's your budget?" promptFunctor={this.setBudget} />
+                }
+            </If>
+            
+            <If test={this.state.results!==undefined}>
+                <ResultView data={this.state.results}/>
+            </If>
+           
+            <If test={this.state.errorMsg !== undefined}>
+                <div className="sorry-Message">{this.state.errorMsg}</div>
+            </If>
+           
+            </div>
+        )
     }
 })
 
@@ -129,6 +435,22 @@ var ContextMap = {
     "sing": SingContext
 };
 
+var Suggestion = React.createClass({
+    
+    doSuggestion: function() {
+        this.props.owner.applySuggestion(this.props.text);
+    },
+    render: function() {
+        return (
+            <li className="col-md-2">
+                <div className="suggestion-value suggestion-button btn btn-default" onClick={this.doSuggestion}>
+                    {this.props.text}
+                </div>
+            </li>
+            )
+    } 
+});
+
 var SearchPage = React.createClass({
     getInitialState: function() {
        return {
@@ -140,21 +462,48 @@ var SearchPage = React.createClass({
     },
     componentWillMount: function() {
         
-        this.setState({selections: [{type: "activity"}], suggestions: ['text', 'eat', 'play']});
+        this.setState({selections: [{type: "activity"}]});
+        
+        this.updateSuggestions(Expected_context_input, this.setContext);
         
     },
     setContext: function(prompt, context) {
       
-        if(ContextMap[context] !== undefined) {
+        var lowerContext = context.toLowerCase();
+      
+        if(ContextMap[lowerContext] !== undefined) {
             
-            prompt.setState({value: context});
+            //prompt.setState({value: lowerContext});
             
-            this.setState({context: ContextMap[context], contextString: context});
+            this.setState({context: ContextMap[lowerContext], contextString: lowerContext});
         }
         else
         {
             this.setState({errorMsg: "Sorry I couldn't understand that. Please try again."});
         }
+    },
+    updateSuggestions: function(data, suggestCallback) {
+        
+        this.suggestCallback = suggestCallback;
+        
+        var suggests = [];
+        
+        for(var i = 0; i < Math.min(6, data.length); i++) {
+            suggests.push(data[i]);
+        }
+        
+        this.setState({suggestions: suggests});  
+    },
+    applySuggestion: function(suggest) {
+      
+      this.suggestCallback(undefined, suggest);
+    },
+    resetContext: function() {
+        
+        this.updateSuggestions(Expected_context_input, this.setContext);
+        
+        console.log('removing');
+        this.setState({context: undefined, contextString: undefined, errorMsg: ""}, function() { console.log('remove');});
     },
     render: function() {
         
@@ -165,41 +514,44 @@ var SearchPage = React.createClass({
         return (
             <div id="search-page" className="full-size">
                 <div id="search-wrapper" className="full-size">
-                    <div className="inner-middle">
-                        <div className="horizontal-middle">
+                    <div className="btn btn-default cancel-search"><span className="glyphicon glyphicon-remove-circle" onClick={this.resetContext}></span></div>
+                    <div className="inner-middle full-size">
+                        <div className="horizontal-middle full-size">
                             
                             { this.state.context === undefined ?
-                                <div className="search-prompt-container">
-                                    <SearchPrompt promptType="" promptFunctor={this.setContext}/>
+                                <div className="search-prompt-container full-size">
+                                    <SearchPrompt promptType="I want to..." promptFunctor={this.setContext}/>
                                      <div>
                                     {error}
                                     </div>
                                 </div>
                                 :
-                                <div className="search-prompt-container">
+                                <div className="search-prompt-container full-size">
                                     <div className="search-box">
                                         <div className="search-title">
                                         I want to { this.state.contextString }
                                         </div>
                                      </div>
-                                     { React.createElement(this.state.context) }
+                                     { React.createElement(this.state.context, {updateSuggestions: this.updateSuggestions}) }
                                 </div>
                             }
                             
-                            <div className="search-suggestions">
-                                <div className="search-suggestions-title">
-                                Here are some suggestions
+                            <If test={ this.state.suggestions.length }>
+                                <div className="search-suggestions">
+                                    <div className="search-suggestions-title">
+                                    Here are some suggestions
+                                    </div>
+                                    <div className="container-fluid">
+                                        <ul className="row suggestions-list">
+                                            
+                                            { this.state.suggestions.map(function(text, i) {
+                                               return <Suggestion text={text} owner={this} key={i}/>
+                                            }.bind(this)) }
+    
+                                        </ul>
+                                    </div>
                                 </div>
-                                <div className="container-fluid">
-                                    <ul className="row suggestions-list">
-                                        
-                                        { this.state.suggestions.map(function(text, i) {
-                                           return <li className="col-md-2 suggestion-value suggestion-button btn btn-default" key={i}>{text}</li>
-                                        }) }
-                                  
-                                    </ul>
-                                </div>
-                            </div>
+                            </If>
                         </div>
                     </div>
                 </div>
